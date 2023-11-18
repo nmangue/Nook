d105e7558214fd126ff46b40cfa67ac7a9c880c9
nmangue <nmangue@users.noreply.github.com>
2023-11-18 12:44:08 +0100

    AspNetCore integration

---
+++ src/Nook.AspNetCore/ComponentBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Nook.Core;
+
+namespace Nook.AspNetCore;
+
+public abstract class ComponentBase<TStore>
+    : ComponentBase where TStore : IStore, new()
+{
+    [Microsoft.AspNetCore.Components.Inject]
+    protected IUse<TStore> Store { get; private set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        Store.StateChanged += Store_StateChanged;
+    }
+
+    private void Store_StateChanged(object? sender, EventArgs e)
+    {
+        if (ShouldRerender())
+        {
+            InvokeAsync(StateHasChanged);
+        }
+    }
+
+    protected virtual bool ShouldRerender() => true;
+}
